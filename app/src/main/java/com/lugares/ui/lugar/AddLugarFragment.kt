c5a58f11b1397925722b388c24529aa31f1fc1b6
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-11 16:21:38 -0600

    Clase 4

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,17 +1,23 @@
 package com.lugares.ui.lugar
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.lugares.R
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.model.Lugar
 import com.lugares.view_models.LugarViewModel
+import java.util.jar.Manifest
 
 
 class AddLugarFragment : Fragment() {
@@ -31,7 +37,46 @@ class AddLugarFragment : Fragment() {
             addLugar()
         }
 
+        locateGPS()
+
         return binding.root
+    }
+
+    private var withPermission = true;
+    private fun locateGPS() {
+        val fusedLocationProviderClient: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        if (
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ), 105)
+        }
+
+        if (withPermission)  {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                location: Location? ->
+                if (location != null) {
+                    binding.tvLatitud.text = "${location.latitude}"
+                    binding.tvLongitud.text = "${location.longitude}"
+                    binding.tvAltura.text = "${location.altitude}"
+                } else {
+                    binding.tvLatitud.text = getString(R.string.error)
+                    binding.tvLongitud.text = getString(R.string.error)
+                    binding.tvAltura.text = getString(R.string.error)
+                }
+            }
+        }
     }
 
     private fun addLugar() {
@@ -41,7 +86,10 @@ class AddLugarFragment : Fragment() {
             var email = binding.etEmail.text.toString()
             var phone = binding.etPhone.text.toString()
             var web = binding.etWebSite.text.toString()
-            var lugar = Lugar(0, name, email, phone, web, 0.0, 0.0, 0.0, "", "")
+            val latitude = binding.tvLatitud.text.toString().toDouble()
+            val length = binding.tvLongitud.text.toString().toDouble()
+            val height = binding.tvAltura.text.toString().toDouble()
+            var lugar = Lugar(0, name, email, phone, web, latitude, length, height, "", "")
             lugarViewModel.addLugar(lugar)
             Toast.makeText(requireContext(), getString(R.string.msg_added), Toast.LENGTH_LONG).show()
         } else {
--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,6 +1,9 @@
 package com.lugares.ui.lugar
 
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -12,6 +15,7 @@ import com.lugares.R
 import com.lugares.databinding.FragmentUpdateLugarBinding
 import com.lugares.model.Lugar
 import com.lugares.view_models.LugarViewModel
+import java.util.jar.Manifest
 
 
 class UpdateLugarFragment : Fragment() {
@@ -34,9 +38,34 @@ class UpdateLugarFragment : Fragment() {
         binding.etPhone.setText(args.lugar.phone)
         binding.etWebSite.setText(args.lugar.web)
 
+        binding.tvLength.text = args.lugar.length.toString()
+        binding.tvLatitude.text = args.lugar.latitude.toString()
+        binding.tvHeight.text = args.lugar.height.toString()
+
         binding.btUpdate.setOnClickListener {
             updateLugar()
         }
+
+        binding.btEmail.setOnClickListener {
+            sendEmail()
+        }
+
+        binding.btPhone.setOnClickListener {
+            makeACall()
+        }
+
+        binding.btWhatsapp.setOnClickListener {
+            sendWhatsApp()
+        }
+
+        binding.btWeb.setOnClickListener {
+            seeWebsite()
+        }
+
+        binding.btLocation.setOnClickListener {
+            seeMap()
+        }
+
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -62,6 +91,82 @@ class UpdateLugarFragment : Fragment() {
         lugarViewModel.updateLugar(lugar)
         Toast.makeText(requireContext(), getString(R.string.msg_updated), Toast.LENGTH_LONG).show()
         findNavController().navigate(R.id.action_updateLugarFragment_to_nav_lugar)
+    }
+
+    private fun sendEmail() {
+        val to = binding.etEmail.text.toString()
+
+        if (to.isNotEmpty()) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "message/rfc822"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(to))
+            intent.putExtra(Intent.EXTRA_SUBJECT,
+                getString(R.string.msg_greetings) + " " + binding.etName.text)
+            intent.putExtra(Intent.EXTRA_TEXT,
+                getString(R.string.msg_email_message))
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_data), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun makeACall() {
+        val phone = binding.etPhone.text.toString()
+
+        if (phone.isNotEmpty()) {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$phone")
+
+            if (requireActivity().checkSelfPermission(android.Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED) {
+                    requireActivity()
+                        .requestPermissions(arrayOf(android.Manifest.permission.CALL_PHONE), 105)
+
+            } else {
+                requireActivity().startActivity(intent)
+            }
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_data), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun sendWhatsApp() {
+        val phone = binding.etPhone.text.toString()
+
+        if (phone.isNotEmpty()) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val uri = "whatsapp://send?phone=506$phone&text=" + getString(R.string.msg_greetings)
+            intent.setPackage("com.whatsapp")
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_data), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun seeWebsite() {
+        val site = binding.etWebSite.text.toString()
+
+        if (site.isNotEmpty()) {
+            val uri = Uri.parse("http://$site")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_data), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun seeMap() {
+        val latitude = binding.tvLatitude.text.toString().toDouble()
+        val length = binding.tvLength.text.toString().toDouble()
+
+        if (latitude.isFinite() && length.isFinite()) {
+            val location = Uri.parse("geo:$latitude,$length?z=18")
+            val intent = Intent(Intent.ACTION_VIEW, location)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_data), Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun deleteLugar() {
